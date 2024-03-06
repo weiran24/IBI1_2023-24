02cef8deaa731e4fb8c68a1f89e43e37c071819c
Weiran <weiran.23@intl.zju.edu.cn>
2024-03-06 15:35:21 +0800

    variables.py

---
+++ Practical4/variables.py
@@ -0,0 +1,22 @@
+a=40
+b=36
+c=30
+d=a - b
+e=b - c
+if d > e :
+    print("the first is better")
+elif d < e:
+    print("the second is better")
+else:
+    print("they are the same")
+#running and strength method is better.
+
+
+x=True
+y=False
+w=x or y
+print(w)
+#when x and y are both true,w is true
+#when x is true y is false,w is true
+#when x is false y is true,w is true
+#when x and y are both false,w is false
