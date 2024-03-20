863b79a6a5de3f87b02951e32d344500660c3fce
weiran24 <161579344+weiran24@users.noreply.github.com>
2024-03-20 16:12:20 +0800

    Create data2.py

---
+++ Practical6/data2.py
@@ -0,0 +1,13 @@
+a=8
+b=6
+c=3.5
+d=2
+e=1
+f=24-a-b-c-d-e
+my_dict={}
+AS={"sleeping":a,"classes":b,"studying":c,"tv":d,"music":e,"other":f}
+s=input("the time of activity you want to know")
+if s in AS :
+    print("the times is AS[s]","hours")
+else :
+    print("wrong")
