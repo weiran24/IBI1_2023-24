7126d31f0fea1d21fe2ed95e24847b895859f1e4
weiran24 <161579344+weiran24@users.noreply.github.com>
2024-04-03 15:32:51 +0800

    Delete repetitive_count.py

--- repetitive_counter.py
+++
@@ -1,8 +0,0 @@
-import re
-seq = 'ATGCAATCGGTGTGTCTGTTCTGAGAGGGCCTAA'
-pattern=r'(GTGTGT|GTCTGT)'
-total=0
-count=r'(?=('+ pattern + r'))'
-matches= re.finditer(count,seq)
-total=sum(1 for _ in matches)
-print(total) 
